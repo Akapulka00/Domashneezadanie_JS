d4d726b344743960cae2bd7053230ab07ce95022
Akapulka01 <svavolhov@gmail.com>
2021-10-20 21:30:55 +0300

    Првые потуги в JS....

---
+++ Pervoe/JS/main1.js
@@ -0,0 +1,6 @@
+"use strict"
+console.log('Второе задание');
+let chislo=56; //Число
+let Sum=Math.floor(chislo/10)+Math.floor(chislo%10);//Сумма чисел //Этото кастыль с Math.floor нужен для отсечение точки инааче сумма была 11,6
+Sum=Sum/1;
+console.log('Сумма  чисел  =',Sum);
