51dbc528fe0f50eb03c15ff22dd5ca38e22ba585
Akapulka00 <svavolhov@gmail.com>
2021-11-10 18:54:33 +0300

    Но 4 же мы делали?

--- SevenHometask/js/main.js
+++ SevenHometask/js/main.js
@@ -30,17 +30,60 @@ let goods = {
 
 function pervoe(obj,from,to){
   let newObj={};
-if(typeof(form)=="number" && typeof(to)=="number" && typeof(obj)=="object"){
+if(typeof(from)=="number" && typeof(to)=="number" && typeof(obj)=="object"){
 for (let elem in obj) {
-  if(obj[elem].price>from && obj[elem].price<price){
+ if(obj[elem].price>from && obj[elem].price<to){
     newObj[elem]=obj[elem];
   }
-  return newObj;
+ 
 }
+return newObj;
+}else{return false}
+}
+//////////////////////////////////////////////////////////////////////////////////
+function vtoroe(obj,title,countToCart){
+  
+if(typeof(countToCart)=="number" && typeof(title)=="string" && typeof(obj)=="object"){
+  for (let elem in obj) {
+    if(obj[elem].title==title){
+       if(obj[elem].count-countToCart>0){
+        obj[elem].count=obj[elem].count-countToCart;
+        return true;
+       }else{
+         console.log("Значение не возможно уменьшить!");
+          return false}
+     }
+    
+   }
+   console.log("Инструмента с таким названием не существует!");
+   return false;
 }else{
-  return false
+  return false;
+}
+}
+/////////////////////////////////////////////////
+let books = [
+  { author: 'Пушкин', title: 'Пир во время чумы'}, 
+  { author: 'Толстой', title: 'Война и мир'}, 
+  { author: 'Лермонтов', title: 'Тамань'}, 
+  { author: 'Гончаров', title: 'Обломов'}, 
+  { author: 'Лермонтов', title: 'Герой Нашего Времени'}, 
+  { author: 'Пушкин', title: 'Руслан и Людмила'}, 
+  { author: 'Лермонтов', title: 'И скучно, и грустно'},
+];
+
+function trete(obj,autor){
+let newBooks=[];
+for(let elem of obj){
+  if(elem.author==autor){
+    newBooks.push(elem)
+  }
+}
+//console.log(newBooks);
+return newBooks;
 }
 
-}
-console.log(pervoe(goods,1,1000));
-pervoe(goods,1,1000);
+
+console.log(pervoe(goods,1000,1300));
+console.log(vtoroe(goods,"Флейтаf",25));
+console.log(trete(books,"Лермонтов"));
